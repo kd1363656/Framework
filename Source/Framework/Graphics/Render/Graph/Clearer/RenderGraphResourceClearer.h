caa050f5abfe1b49c2f4f1335781a798a9214071
kd1363656 <kd1363656@st.kobedenshi.ac.jp>
2026-06-21 23:30:21 +0900

    レンダーグラフのせきにんを分割するためクラス分け

---
+++ Source/Framework/Graphics/Render/Graph/Clearer/RenderGraphResourceClearer.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+namespace FWK::Grpahics
+{
+	class RenderGraphResourceClearer final
+	{
+	public:
+
+		 RenderGraphResourceClearer() = default;
+		~RenderGraphResourceClearer() = default;
+
+		RenderGraphResourceClearer(const RenderGraphResourceClearer&)			= delete;
+		RenderGraphResourceClearer(	     RenderGraphResourceClearer&&) noexcept = default;
+		
+		RenderGraphResourceClearer& operator=(const RenderGraphResourceClearer&)		   = delete;
+		RenderGraphResourceClearer& operator=(		RenderGraphResourceClearer&&) noexcept = default;
+
+	private:
+
+	};
+}
