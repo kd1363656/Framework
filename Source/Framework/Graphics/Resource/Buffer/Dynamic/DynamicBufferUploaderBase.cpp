f10a77c624ecea1fc8cf46c51359c55f1b41536e
kd1363656 <kd1363656@st.kobedenshi.ac.jp>
2026-06-30 23:54:52 +0900

    基底クラス作成中

--- Source/Framework/Graphics/Resource/Buffer/Dynamic/DynamicBufferUploaderBase.cpp
+++ Source/Framework/Graphics/Resource/Buffer/Dynamic/DynamicBufferUploaderBase.cpp
@@ -1,8 +1,54 @@
 ﻿#include "DynamicBufferUploaderBase.h"
 
-FWK::Graphics::DynamicBufferUploaderBase::DynamicBufferUploaderBase(const UINT64& a_typeSize) : 
-	k_typeSize(a_typeSize),
+FWK::Graphics::DynamicBufferUploaderBase::DynamicBufferUploaderBase(const UINT64& a_bufferTypeSize) : 
+	k_typeSize(a_bufferTypeSize),
 
-	m_currentOffset(k_initialOffset)
+	m_uploadBuffer(),
+
+	m_createCount(k_invalidCreateCount),
+
+	m_currentElementIndex(k_initialElementBufferIndex)
 {}
 FWK::Graphics::DynamicBufferUploaderBase::~DynamicBufferUploaderBase() = default;
+
+void FWK::Graphics::DynamicBufferUploaderBase::Deserialize(const nlohmann::json& a_rootJson)
+{
+	if (a_rootJson.is_null()) { return; }
+
+	m_jsonConverter.Deserialize(a_rootJson, *this);
+}
+
+void FWK::Graphics::DynamicBufferUploaderBase::BeginFrame()
+{
+	m_currentElementIndex = k_initialElementBufferIndex;
+}
+
+nlohmann::json FWK::Graphics::DynamicBufferUploaderBase::Serialize() const
+{
+	return m_jsonConverter.Serialize(*this);
+}
+
+bool FWK::Graphics::DynamicBufferUploaderBase::CreateUploadBuffer(const Device& a_device, const UINT64& a_alignment)
+{
+	FWK_ASSERT_RETURN_VALUE_IF(m_createCount == k_invalidCreateCount, "バッファの作成個数が0のため、作成処理に失敗しました。", false);
+	
+	// 送る定数バッファの型サイズを256バイトにアライメントする
+	const auto& l_alignedTypeSize = Utility::AlignUp(k_typeSize, a_alignment);
+	const auto& l_totalSize       = m_createCount * l_alignedTypeSize;
+
+	FWK_ASSERT_RETURN_VALUE_IF(l_totalSize == Constant::k_invalidBufferSize,  "作成バッファーサイズが0のためバッファの生成処理に失敗しました。", false);
+	FWK_ASSERT_RETURN_VALUE_IF(!m_uploadBuffer.Create(a_device, l_totalSize), "バッファの生成処理に失敗しました。", false);
+
+	return true;
+}
+
+UINT64 FWK::Graphics::DynamicBufferUploaderBase::AllocateElementRange(const UINT64& a_elementCount)
+{
+	const auto l_startElementIndex = m_currentElementIndex;
+
+	m_currentElementIndex += a_elementCount;
+
+	FWK_ASSERT_RETURN_VALUE_IF(m_createCount < m_currentElementIndex, "要素範囲を超えており、Allocate処理に失敗しました。", k_invalidElementBufferIndex);
+
+	return l_startElementIndex;
+}
--- Source/Framework/Graphics/Resource/Buffer/Dynamic/DynamicBufferUploaderBase.h
+++ Source/Framework/Graphics/Resource/Buffer/Dynamic/DynamicBufferUploaderBase.h
@@ -6,11 +6,11 @@ namespace FWK::Graphics
 	{
 	public:
 
-		explicit DynamicBufferUploaderBase(const UINT64& a_typeSize);
+		explicit DynamicBufferUploaderBase(const UINT64& a_bufferTypeSize);
 		virtual ~DynamicBufferUploaderBase();
 
-		void Deserialize(const nlohmann::json& a_rootJson);
-		bool Create	    (const Device&		   a_device);
+		void		 Deserialize(const nlohmann::json& a_rootJson);
+		virtual bool Create	    (const Device&		   a_device) = 0;
 
 		void BeginFrame();
 		
@@ -20,20 +20,31 @@ namespace FWK::Graphics
 
 		static constexpr auto& GetREFInvalidCreateCount() { return k_invalidCreateCount; }
 
+		const auto& GetREFTypeSize() const { return k_typeSize; }
+
 		const auto& GetREFCreateCount() const { return m_createCount; }
+
+	protected:
+
+		bool CreateUploadBuffer(const Device& a_device, const UINT64& a_alignedSize);
+
+		UINT64 AllocateElementRange(const UINT64& a_elementCount);
 
 	private:
 
-		static constexpr UINT64 k_invalidCreateCount = 0ULL;
-		static constexpr UINT64 k_initialOffset      = 0ULL;
+		static constexpr UINT64 k_invalidCreateCount        = 0ULL;
+		static constexpr UINT64 k_initialElementBufferIndex = 0ULL;
+		static constexpr UINT64 k_invalidElementBufferIndex = std::numeric_limits<UINT>::max();
 
 		const UINT64 k_typeSize;
 
 		UploadBuffer m_uploadBuffer;
 
+		Converter::DynamicBufferUploaderBaseJsonConverter m_jsonConverter = {};
+
 		UINT64 m_createCount;
 
-		UINT64 m_currentOffset;
+		UINT64 m_currentElementIndex;
 
 		FWK_DEFINE_TYPE_INFO_ROOT(DynamicBufferUploaderBase)
 	};
