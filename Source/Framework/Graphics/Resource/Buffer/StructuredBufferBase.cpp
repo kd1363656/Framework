dbc2de9398d794bb8f8f347484a2797bd815ec2f
kd1363656 <kd1363656@st.kobedenshi.ac.jp>
2026-07-09 09:37:17 +0900

    大規模なリファクタリングを行う

---
+++ Source/Framework/Graphics/Resource/Buffer/StructuredBufferBase.cpp
@@ -0,0 +1,21 @@
+﻿//#include "StructuredBufferBase.h"
+//
+//FWK::Graphics::StructuredBufferBase::StructuredBufferBase(StructuredBufferBase&& a_other) noexcept
+//{
+//	MoveFrom(std::move(a_other));
+//}
+//
+//FWK::Graphics::StructuredBufferBase& FWK::Graphics::StructuredBufferBase::operator=(StructuredBufferBase&& a_other) noexcept
+//{
+//	if (this == &a_other) { return *this; }
+//
+//	MoveFrom(std::move(a_other));
+//
+//	return *this;
+//}
+//
+//void FWK::Graphics::StructuredBufferBase::ReleaseImmediatelySRVDescriptorIndex(TypeAlias::CBVSRVUAVDescriptorPool& a_cbvSRVUAVDescriptorPool)
+//{
+//
+//}
+//
--- Source/Framework/Graphics/Resource/Buffer/StructuredBufferBase.h
+++ Source/Framework/Graphics/Resource/Buffer/StructuredBufferBase.h
@@ -1,15 +1,55 @@
-﻿#pragma once
-
-namespace FWK::Graphics
-{
-	class StructuredBufferBase
-	{
-	public:
-
-		         StructuredBufferBase() = default;
-		virtual ~StructuredBufferBase() = default;
-
-	private:
-
-	};
-}
+﻿//#pragma once
+//
+//namespace FWK::Graphics
+//{
+//	class StructuredBufferBase
+//	{
+//	public:
+//
+//		         StructuredBufferBase() = default;
+//		virtual ~StructuredBufferBase() = default;
+//
+//		StructuredBufferBase(const StructuredBufferBase&) = delete;
+//		StructuredBufferBase(      StructuredBufferBase&& a_other) noexcept;
+//
+//		StructuredBufferBase& operator=(const StructuredBufferBase&) = delete;
+//		StructuredBufferBase& operator=(      StructuredBufferBase&& a_other) noexcept;
+//
+//		virtual bool ReserveRelease(const UINT64& a_retiredFenceValue, ResourceReleaseContext& a_resourceReleaseContext) = 0;
+//
+//		virtual void Relase() = 0;
+//
+//		void ReleaseImmediatelySRVDescriptorIndex(TypeAlias::CBVSRVUAVDescriptorPool& a_cbvSRVUAVDescriptorPool);
+//
+//		const auto& GetREFBufferGPUResource() const { return m_bufferGPUResource; }
+//
+//		auto GetVALSRVDescriptorIndex() const { return m_srvDescriptorIndex; }
+//
+//	protected:
+//
+//		TypeAlias::DescriptorIndex CreateSRV(const Device&                             a_device,
+//											 const Struct::GPUResource&                a_bufferGPUResource,
+//											 const UINT                                a_elementCount,
+//			                                 const UINT				                   a_structureByteStride,
+//			                                       TypeAlias::CBVSRVUAVDescriptorPool& a_cbvSRVUAVDescriptorPool);
+//
+//		bool ReserveReleaseCurrentResource(const UINT64& a_retiredFenceValue, ResourceReleaseContext& a_resourceReleaseContext);
+//
+//		void SetBufferGPUResource(Struct::GPUResource&& a_set) { m_bufferGPUResource = std::move(a_set); }
+//
+//		void SetSRVDescriptorIndex(const TypeAlias::DescriptorIndex a_set);
+//
+//		static constexpr UINT64 k_maxStructuredBufferElementCount = std::numeric_limits<UINT>::max();
+//
+//	private:
+//
+//		void MoveFrom(StructuredBufferBase&& a_other) noexcept;
+//
+//		static constexpr UINT k_invalidElementCount         = 0U;
+//		static constexpr UINT k_invalidStructuredByteStride = 0U;
+//
+//		Struct::GPUResource m_bufferGPUResource = {};
+//
+//		TypeAlias::DescriptorIndex m_srvDescriptorIndex = Constant::k_invalidDescriptorIndex;
+//	};
+//}
