eb6c91a2e03d2bfb1c1b906265e8b9a7b2fe0cb3
kd1363656 <kd1363656@st.kobedenshi.ac.jp>
2026-07-15 10:24:06 +0900

    ボーン行列計算CSの作成中

---
+++ Asset/Shader/Model/Skeletal/SkeletalAnimationLocalMatrix_CS.hlsl
@@ -0,0 +1,25 @@
+﻿#include "SkeletalAnimation.hlsli"
+
+cbuffer CBSkeletalAnimationLocalMatrix : register(b0)
+{
+    float g_blendWeight;
+    float g_animationTimeSecond;
+    float g_blendTargetAnimationTimeSecond;
+    
+    uint g_boneBufferSRVDescriptorIndex;
+    uint g_motionSequenceBufferSRVDescriptorIndex;
+    uint g_boneMotionTrackBufferSRVDescriptorIndex;
+    uint g_keyFrameBufferSRVDescriptorIndex;
+    uint g_boneMatrixBufferUAVDescriptorIndex;
+    
+    uint g_motionIndex;
+    uint g_blendTargetMotionIndex;
+    uint g_boneCount;
+    uint g_isBlending;
+}
+
+[numthreads(1, 1, 1)]
+void main( uint3 DTid : SV_DispatchThreadID )
+{
+    
+}
--- Source/Framework/Definition/Struct/Graphics/Buffer/Constant/CBSkeletalAnimationLocalMatrixStruct.h
+++ Source/Framework/Definition/Struct/Graphics/Buffer/Constant/CBSkeletalAnimationLocalMatrixStruct.h
@@ -4,20 +4,19 @@ namespace FWK::Struct
 {
 	struct  CBSkeletalAnimationLocalMatrix final
 	{
+		float m_blendWeight                    = Graphics::SkeletalAnimationPlayer::k_initialBlendWeight;
+		float m_animationTimeSecond            = Graphics::SkeletalAnimationModelRecord::k_initialAnimationTimeSecond;
+		float m_blendTargetAnimationTimeSecond = Graphics::SkeletalAnimationModelRecord::k_initialAnimationTimeSecond;
+
 		TypeAlias::DescriptorIndex m_boneBufferSRVDescriptorIndex            = Graphics::DescriptorHeap::k_invalidDescriptorIndex;
 		TypeAlias::DescriptorIndex m_motionSequenceBufferSRVDescriptorIndex  = Graphics::DescriptorHeap::k_invalidDescriptorIndex;
 		TypeAlias::DescriptorIndex m_boneMotionTrackBufferSRVDescriptorIndex = Graphics::DescriptorHeap::k_invalidDescriptorIndex;
 		TypeAlias::DescriptorIndex m_keyFrameBufferSRVDescriptorIndex        = Graphics::DescriptorHeap::k_invalidDescriptorIndex;
 		TypeAlias::DescriptorIndex m_boneMatrixBufferUAVDescriptorIndex      = Graphics::DescriptorHeap::k_invalidDescriptorIndex;
 
-		float m_blendWeight                    = Graphics::SkeletalAnimationPlayer::k_initialBlendWeight;
-		float m_animationTimeSecond            = Graphics::SkeletalAnimationModelRecord::k_initialAnimationTimeSecond;
-		float m_blendTargetAnimationTimeSecond = Graphics::SkeletalAnimationModelRecord::k_initialAnimationTimeSecond;
-
 		std::uint32_t m_motionIndex            = Graphics::SkeletalAnimationPlayer::Animation::k_invalidMotionIndex;
 		std::uint32_t m_blendTargetMotionIndex = Graphics::SkeletalAnimationPlayer::Animation::k_invalidMotionIndex;
 		std::uint32_t m_boneCount              = Converter::SkeletalAnimationModelBinaryConverter::k_emptyBoneCount;
-
-		std::uint32_t m_isBlending = static_cast<std::uint32_t>(false);
+		std::uint32_t m_isBlending             = static_cast<std::uint32_t>(false);
 	};
 }
