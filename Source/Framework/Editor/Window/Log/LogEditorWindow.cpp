c2f9d5c797fbeb344f1431abf12880458c9382d4
kd1363656 <kd1363656@st.kobedenshi.ac.jp>
2026-08-16 12:16:58 +0900

    ログ改良中

--- Source/Framework/Editor/Window/Log/LogEditorWindow.cpp
+++ Source/Framework/Editor/Window/Log/LogEditorWindow.cpp
@@ -32,6 +32,54 @@ void FWK::Editor::LogEditorWindow::Draw()
 void FWK::Editor::LogEditorWindow::AddLog(const TypeAlias::Math::Color& a_textColor, const std::string& a_text)
 {
 	// 空モッジの場合は追加するものがないため終了する
+	if (a_text.empty()) { return; }
+
+	// 現在のTextBufferサイズ
+	// この位置から今回の新しいログが追加される
+	int l_oldSize = m_textBuffer.size();
+
+	// 直前のログが改行で終わっている場合
+	// m_textLineOffsetsの最後 == 現在のTextuBufferサイズとなり、
+	// 「まだ何も書かれていない次の行」を表している
+	// その行へ今回のログを書き始めるため
+	// その行の色を今回指定された色へ変更する
+	if (m_textLineOffsets.Size > NULL &&
+		!m_textLineColorList.empty())
+	{
+		const int l_lastLineIndex = m_textLineOffsets.Size - k_nextLineIndexOffset;
+
+		if (m_textLineOffsets[l_lastLineIndex] == l_oldSize)
+		{
+			m_textLineColorList[static_cast<std::size_t>(l_lastLineIndex)] = a_textColor;
+		}
+	}
+
+	// EditorManager側ですでに完成しているログ文字r津を
+	// ImGuiTextBufferへそのまま追加する
+	// append()へ終端位置も渡しているので
+	// std::string内の指定範囲だけを正確に追加できる
+	m_textBuffer.append(a_text.data(), a_text.data() + a_text.size());
+
+	// 今回追加されてた文字列の範囲だけ走査して、
+	// 新しい行の開始位置を記録する
+	for (const int l_newSize = m_textBuffer.size(); l_oldSize < l_newSize; ++l_oldSize)
+	{
+		if (m_textBuffer[l_oldSize] != '\n') { continue; }
+
+		// 改行の次の位置が次行の開始位置
+		m_textLineOffsets.push_back(l_oldSize + k_nextLineStartOffset);
+
+		// 今回追加した一つのログは、
+		// 複数行になっていてもすべて同じ色として扱う
+		m_textLineColorList.push_back(a_textColor);
+	}
+
+	// 自動スクロールが有効なら
+	// 次回DrawLog()時に一番下まで移動する
+	if (m_canAutoScroll)
+	{
+		m_canScrollToBottom = true;
+	}
 }
 
 void FWK::Editor::LogEditorWindow::DrawEditorOptions()
@@ -68,7 +116,7 @@ void FWK::Editor::LogEditorWindow::DrawEditorOptions()
 		ImGui::LogToClipboard();
 	}
 
-	ImGui::SameLine();
+	ImGui::SameLine  ();
 	m_textFilter.Draw(k_optionFilterString.data(), k_filterInputWidth);
 
 	const ImVec2 l_size = {};
@@ -93,16 +141,28 @@ void FWK::Editor::LogEditorWindow::DrawLog()
 	// フィルターが有効かどうかを確認
 	if (m_textFilter.IsActive())
 	{
-		for (int l_lineNum = 0; l_lineNum < m_textLineOffsets.Size; l_lineNum++)
+		for (int l_lineNUM = 0; l_lineNUM < m_textLineOffsets.Size; ++l_lineNUM)
 		{
-			const char* l_lineStart = l_buffer + m_textLineOffsets[l_lineNum];
-			const char* l_lineEnd   = (l_lineNum + k_nextLineIndexOffset < m_textLineOffsets.Size) ? (l_buffer + m_textLineOffsets[l_lineNum + k_nextLineIndexOffset] + k_excludeNewLineOffset) : l_bufferEnd;
+			const char* l_lineStart = l_buffer + m_textLineOffsets[l_lineNUM];
+			const char* l_lineEnd   = (l_lineNUM + k_nextLineIndexOffset < m_textLineOffsets.Size) ? (l_buffer + m_textLineOffsets[l_lineNUM + k_nextLineIndexOffset] + k_excludeNewLineOffset) : l_bufferEnd;
 
 			// フィルター条件に合わない行はスキップ
 			if (!m_textFilter.PassFilter(l_lineStart , l_lineEnd)) { continue; }
 
+			const auto& l_textColor = m_textLineColorList[static_cast<std::size_t>(l_lineNUM)];
+
+			// FWK側ではDirectX::SimpleMath::Colorを使用しているため
+			// ImGuiへ渡す直前だけImVec4へ変換する
+			const ImVec4& l_imGuiTextColor = { l_textColor.R(),
+											   l_textColor.G(),
+											   l_textColor.B(),
+											   l_textColor.A() };
+
+			ImGui::PushStyleColor(ImGuiCol_Text, l_imGuiTextColor);
+
 			// 合致する行だけ表示
-			ImGui::Text(l_lineStart , l_lineEnd);
+			ImGui::TextUnformatted(l_lineStart, l_lineEnd);
+			ImGui::PopStyleColor  ();
 		}
 	}
 	else
@@ -114,14 +174,24 @@ void FWK::Editor::LogEditorWindow::DrawLog()
 
 		while (l_clipper.Step())
 		{
-			for (int l_lineNum = l_clipper.DisplayStart; l_lineNum < l_clipper.DisplayEnd; l_lineNum++)
+			for (int l_lineNUM = l_clipper.DisplayStart; l_lineNUM < l_clipper.DisplayEnd; l_lineNUM++)
 			{
-				const char* l_lineStart = l_buffer + m_textLineOffsets[l_lineNum];
-				const char* l_lineEnd   = (l_lineNum + k_nextLineIndexOffset < m_textLineOffsets.Size) ? (l_buffer + m_textLineOffsets[l_lineNum + k_nextLineIndexOffset] + k_excludeNewLineOffset) : l_bufferEnd;
+				const char* l_lineStart = l_buffer + m_textLineOffsets[l_lineNUM];
+				const char* l_lineEnd   = (l_lineNUM + k_nextLineIndexOffset < m_textLineOffsets.Size) ? (l_buffer + m_textLineOffsets[l_lineNUM + k_nextLineIndexOffset] + k_excludeNewLineOffset) : l_bufferEnd;
 
-				ImGui::Text(l_lineStart , l_lineEnd);
+				const auto& l_textColor = m_textLineColorList[static_cast<std::size_t>(l_lineNUM)];
+
+				const ImVec4& l_imGuiTextColor = { l_textColor.R(),
+											       l_textColor.G(),
+											       l_textColor.B(),
+											       l_textColor.A() };
+
+				ImGui::PushStyleColor (ImGuiCol_Text, l_imGuiTextColor);
+				ImGui::TextUnformatted(l_lineStart,   l_lineEnd);
+				ImGui::PopStyleColor  ();
 			}
 		}
+
 		l_clipper.End();
 	}
 
@@ -130,6 +200,7 @@ void FWK::Editor::LogEditorWindow::DrawLog()
 	if (m_canScrollToBottom)
 	{
 		ImGui::SetScrollHereY(k_scrollToBottomRatio);
+
 		m_canScrollToBottom = false;
 	}
 
@@ -138,9 +209,12 @@ void FWK::Editor::LogEditorWindow::DrawLog()
 
 void FWK::Editor::LogEditorWindow::ClearLog()
 {
-	m_textBuffer.clear     ();
-	m_textLineOffsets.clear();
+	m_textBuffer.clear       ();
+	m_textLineOffsets.clear  ();
+	m_textLineColorList.clear();
 
 	// 一行目の開始位置を保証するためにpush_back
 	m_textLineOffsets.push_back(k_ensureFirstLine);
+
+	m_textLineColorList.emplace_back(Constant::k_debugDefaultColor);
 }
