2fd110bffd7a258e9e70a27a825d590d259bacfb
kd1363656 <kd1363656@st.kobedenshi.ac.jp>
2026-08-22 00:59:02 +0900

    お次はカメラコンポーネントの作成

---
+++ Source/Framework/Component/Camera/CameraComponent.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+namespace FWK
+{
+	class CameraComponent final : public FWK::ComponentBase
+	{
+	public:
+
+		 CameraComponent()          = default;
+		~CameraComponent() override = default;
+
+	private:
+
+		FWK_DEFINE_TYPE_INFO(CameraComponent, ComponentBase)
+	};
+}
