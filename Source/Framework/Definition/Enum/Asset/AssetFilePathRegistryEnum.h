79c0c1526b2f9d109f6259102b45ff665383cf9f
kd1363656 <kd1363656@st.kobedenshi.ac.jp>
2026-09-03 17:47:57 +0900

    ファイルパス管理方法を変更

---
+++ Source/Framework/Definition/Enum/Asset/AssetFilePathRegistryEnum.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+namespace FWK::Enum
+{
+	enum class AssetFilePathRegistryType
+	{
+		Invalid,
+		Prefab,
+		Scene,
+	};
+
+	FWK_JSON_SERIALIZE_ENUM
+	(
+		AssetFilePathRegistryType,
+		FWK_JSON_ENUM_VALUE(AssetFilePathRegistryType::Invalid),
+		FWK_JSON_ENUM_VALUE(AssetFilePathRegistryType::Prefab),
+		FWK_JSON_ENUM_VALUE(AssetFilePathRegistryType::Scene),
+	)
+}
--- Source/Framework/Framework.h
+++ Source/Framework/Framework.h
@@ -577,7 +577,9 @@
 #include "Editor/Window/Outliner/WorldOutlinerEditorWindow.h"
 #include "Utility/IMGUI/Tooltip/IMGUITooltipUtility.h"
 #include "Editor/Window/Details/DetailsEditorWindow.h"
-#include "Definition/Enum/Editor/Window/ContentBrowserEditorWindowEnum.h"
+
+#include "Definition/Enum/Asset/AssetFilePathRegistryEnum.h"
+
 #include "Utility/File/SaveFileDialogUtility.h"
 #include "Editor/Window/ContentBrowser/Asset/Converter/Json/ContentBrowserEditorWindowAssetRegistryJsonConverter.h"
 #include "Editor/Window/ContentBrowser/Asset/ContentBrowserEditorWindowAssetRegistry.h"
